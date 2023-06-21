6a555e702edd5eacd29966fb222ad6ddc726a2ce
LAB-05 <LAB-05@PC513>
2023-06-21 12:47:15 -0500

    FinalCompleto

---
+++ src/main/java/ec/edu/ups/trabajoedd/TrabajoEDD.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.trabajoedd;
+
+
+/**
+ *
+ * @author LAB-05
+ */
+public class TrabajoEDD {
+
+    public static void main(String[] args) {
+        Cola cola = new Cola();
+        cola.agregarCola();
+        cola.ordenar();
+
+    
+}
+}
